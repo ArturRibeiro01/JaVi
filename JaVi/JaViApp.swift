ad3a390d1d301fa9c6033bb5cfc01e6ea35d3cec
art-Artur Ribeiro <profissional.arturrc@gmail.com>
2025-03-23 01:59:19 -0300

    Initial Commit

---
+++ JaVi/JaViApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaViApp.swift
+//  JaVi
+//
+//  Created by Artur Mac on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct JaViApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
